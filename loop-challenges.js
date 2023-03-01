c773c66e00ae8ee9e40ccf4249b570836101c32c
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-03-01 15:38:54 -0600

    initial commit

---
+++ loop-challenges.js
@@ -0,0 +1,18 @@
+// Loop 1:
+
+    for(var a=0; a<=20; a++)
+        // console.log(a);
+    if (a%2 != 0){
+        console.log(a)
+    }
+
+//Loop 2:
+    for(var a=100; a>=0; a--)
+        // console.log(a);
+    if(a%3 == 0){
+        console.log(a);
+    }
+
+//Loop 3:
+
+    
